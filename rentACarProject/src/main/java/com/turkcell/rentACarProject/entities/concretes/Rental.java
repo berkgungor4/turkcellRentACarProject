9cf7cc638989d537c75fc76ba656152378b09080
berkgungor4 <77715403+berkgungor4@users.noreply.github.com>
2022-03-07 14:52:57 +0300

    04.03.2022 workshop

---
+++ rentACarProject/src/main/java/com/turkcell/rentACarProject/entities/concretes/Rental.java
@@ -0,0 +1,42 @@
+package com.turkcell.rentACarProject.entities.concretes;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="rentals")
+public class Rental {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    
+    @Column(name = "rentDate")
+    private LocalDate rentDate;
+    
+    @Column(name = "returnDate")
+    private LocalDate returnDate;
+    
+    @Column(name = "customerId")
+    private int customerId;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+}
