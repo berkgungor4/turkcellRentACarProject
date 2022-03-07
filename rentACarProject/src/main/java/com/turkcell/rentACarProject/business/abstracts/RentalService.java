cc228c8deb343d6f888e247a78637a98a96c5276
berkgungor4 <77715403+berkgungor4@users.noreply.github.com>
2022-03-07 15:09:37 +0300

    04.03.2022 workshop

---
+++ rentACarProject/src/main/java/com/turkcell/rentACarProject/business/abstracts/RentalService.java
@@ -0,0 +1,25 @@
+package com.turkcell.rentACarProject.business.abstracts;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sun.net.httpserver.Authenticator.Result;
+import com.turkcell.rentACarProject.business.dtos.rental.GetRentalDto;
+import com.turkcell.rentACarProject.business.dtos.rental.ListRentalDto;
+import com.turkcell.rentACarProject.business.requests.rental.CreateRentalRequest;
+import com.turkcell.rentACarProject.business.requests.rental.DeleteRentalRequest;
+import com.turkcell.rentACarProject.business.requests.rental.UpdateRentalRequest;
+import com.turkcell.rentACarProject.core.utilities.result.DataResult;
+
+@Service
+public interface RentalService {
+	
+	DataResult<List<ListRentalDto>> getAll();
+	DataResult<GetRentalDto> getById(int id);
+	DataResult<List<GetRentalDto>> getByCarId(int id);
+	
+    Result add(CreateRentalRequest createRentalRequest);
+    Result update(UpdateRentalRequest updateRentalRequest);
+    Result delete(DeleteRentalRequest deleteRentalRequest);
+}
