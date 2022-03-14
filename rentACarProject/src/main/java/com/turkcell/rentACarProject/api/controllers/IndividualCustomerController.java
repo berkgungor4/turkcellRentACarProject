5449cf036caf9ac9e7124fbe722907aa5ba1d538
berkgungor4 <77715403+berkgungor4@users.noreply.github.com>
2022-03-14 18:20:16 +0300

    Update

--- rentACarProject/src/main/java/com/turkcell/rentACarProject/api/controllers/IndividualCustomerController.java
+++ rentACarProject/src/main/java/com/turkcell/rentACarProject/api/controllers/IndividualCustomerController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.turkcell.rentACarProject.business.abstracts.IndividualCustomerService;
 import com.turkcell.rentACarProject.business.dtos.individualCustomer.ListIndividualCustomerDto;
 import com.turkcell.rentACarProject.business.requests.individualCustomer.CreateIndividualCustomerRequest;
-import com.turkcell.rentACarProject.core.exceptions.BusinessException;
 import com.turkcell.rentACarProject.core.utilities.results.DataResult;
 import com.turkcell.rentACarProject.core.utilities.results.Result;
 
@@ -39,7 +38,7 @@ public class IndividualCustomerController {
 		
 	}
 	@PostMapping("/create")
-	Result create(@RequestBody CreateIndividualCustomerRequest createIndividualCustomerRequest) throws BusinessException {
+	Result create(@RequestBody CreateIndividualCustomerRequest createIndividualCustomerRequest) {
 		return this.individualCustomerService.create(createIndividualCustomerRequest);
 		
 	}
