c38fd5d95e48b4a4dc376d9195a7e95a614b1131
berkgungor4 <77715403+berkgungor4@users.noreply.github.com>
2022-03-14 18:19:40 +0300

    Update

--- rentACarProject/src/main/java/com/turkcell/rentACarProject/business/abstracts/IndividualCustomerService.java
+++ rentACarProject/src/main/java/com/turkcell/rentACarProject/business/abstracts/IndividualCustomerService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.turkcell.rentACarProject.business.dtos.individualCustomer.ListIndividualCustomerDto;
 import com.turkcell.rentACarProject.business.requests.individualCustomer.CreateIndividualCustomerRequest;
-import com.turkcell.rentACarProject.core.exceptions.BusinessException;
 import com.turkcell.rentACarProject.core.utilities.results.DataResult;
 import com.turkcell.rentACarProject.core.utilities.results.Result;
 
@@ -12,5 +11,5 @@ public interface IndividualCustomerService {
 	
 	DataResult<List<ListIndividualCustomerDto>> getAll();
 	DataResult<ListIndividualCustomerDto> getById(int id);
-	Result create(CreateIndividualCustomerRequest createIndividualCustomerRequest) throws BusinessException;
+	Result create(CreateIndividualCustomerRequest createIndividualCustomerRequest);
 }
