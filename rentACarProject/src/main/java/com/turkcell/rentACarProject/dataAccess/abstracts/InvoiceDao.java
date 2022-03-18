801d4bf840800604c186866e15368feddb132cc2
berkgungor4 <77715403+berkgungor4@users.noreply.github.com>
2022-03-18 18:07:15 +0300

    14.03.2022 Workshop

--- rentACarProject/src/main/java/com/turkcell/rentACarProject/dataAccess/abstracts/InvoiceDao.java
+++ rentACarProject/src/main/java/com/turkcell/rentACarProject/dataAccess/abstracts/InvoiceDao.java
@@ -3,9 +3,9 @@ package com.turkcell.rentACarProject.dataAccess.abstracts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.turkcell.rentACarProject.entities.concretes.IndividualCustomer;
+import com.turkcell.rentACarProject.entities.concretes.Invoice;
 
 @Repository
-public interface IndividualCustomerDao extends JpaRepository<IndividualCustomer, Integer> {
-
+public interface InvoiceDao extends JpaRepository<Invoice, Integer>{
+	
 }
--- rentACarProject/src/main/java/com/turkcell/rentACarProject/dataAccess/abstracts/OrderedAdditionalServiceDao.java
+++ rentACarProject/src/main/java/com/turkcell/rentACarProject/dataAccess/abstracts/OrderedAdditionalServiceDao.java
@@ -3,9 +3,11 @@ package com.turkcell.rentACarProject.dataAccess.abstracts;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.turkcell.rentACarProject.entities.concretes.OrderedAdditionalService;
 
+@Repository
 public interface OrderedAdditionalServiceDao extends JpaRepository<OrderedAdditionalService, Integer>{
 	
 	List<OrderedAdditionalService> findAllByRentalId(int RentalId);
