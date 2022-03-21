06edeb1a12dc20cabe827b1ce5295051498f2be9
berkgungor4 <77715403+berkgungor4@users.noreply.github.com>
2022-03-21 17:54:35 +0300

    18.03.2022 workshop

--- rentACarProject/src/main/java/com/turkcell/rentACarProject/dataAccess/abstracts/BrandDao.java
+++ rentACarProject/src/main/java/com/turkcell/rentACarProject/dataAccess/abstracts/BrandDao.java
@@ -2,10 +2,12 @@ package com.turkcell.rentACarProject.dataAccess.abstracts;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.turkcell.rentACarProject.entities.concretes.Brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BrandDao extends JpaRepository<Brand, Integer>{
@@ -14,3 +16,5 @@ public interface BrandDao extends JpaRepository<Brand, Integer>{
 	
 	List<Brand> getBrandByName(String name);
 }
+
+
---
+++ rentACarProject/src/main/java/com/turkcell/rentACarProject/dataAccess/abstracts/PaymentDao.java
@@ -0,0 +1,14 @@
+package com.turkcell.rentACarProject.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.turkcell.rentACarProject.entities.concretes.Payment;
+
+@Repository
+public interface PaymentDao extends JpaRepository<Payment, Integer> {
+
+	Payment getByPaymentId(int paymentId);
+	Payment getByInvoice_invoiceId(int invoiceId);
+	Payment getByOrderedAdditionalService_orderedAdditionalServiceId(int orderedAdditionalServiceId);
+}
