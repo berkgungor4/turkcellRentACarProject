7063830a980312d6bf8dd51432672897929a0fe1
berkgungor4 <berkgngr@hotmail.com>
2022-04-01 19:10:06 +0300

    LastUpdate

--- rentACarProject/src/main/java/com/turkcell/rentACarProject/core/posServices/FakeIsBank.java
+++
@@ -1,9 +0,0 @@
-package com.turkcell.rentACarProject.core.posServices;
-
-public class FakeIsBank {
-	
-	public boolean payments(String cardNumber, String cardOwnerName, int cardCvvNumber) {
-		return true;
-	}
-	
-}
